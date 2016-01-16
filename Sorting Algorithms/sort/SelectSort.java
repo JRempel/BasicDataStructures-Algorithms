6b2df84378ac32a6d1683285cb9e1fc5b9ffa3d1
JRempel <Jonathan.Rempel@dal.ca>
2016-01-16 15:55:50 -0400

    Added brief comments

--- Sorting Algorithms/sort/SelectSort.java
+++ Sorting Algorithms/sort/SelectSort.java
@@ -9,33 +9,39 @@ public class SelectSort {
 		else {
 			T temp;
 			int index;
-			for (int i = 0; i < input.length; i++) {
+			// repeat the search & swap n-1 times
+			for (int i = 0; i < input.length - 1; i++) {
 				index = i;
+				// scan for the smallest key index in the remaining items
 				for (int j = i + 1; j < input.length; j++) {
 					if (input[j].compareTo(input[i]) < 0) {
 						index = j;
 					}
 				}
+				// swap smallest item with item at current index
 				temp = input[i];
 				input[i] = input[index];
 				input[index] = temp;
 			}
 		}
 	}
-	
+
 	public static <T extends Comparable<T>> void SelectionSort(ArrayList<T> input) {
 		if (input.size() <= 1)
 			return;
 		else {
 			T temp;
 			int index;
-			for (int i = 0; i < input.size(); i++) {
+			// repeat the search & swap n-1 times
+			for (int i = 0; i < input.size() - 1; i++) {
 				index = i;
+				// scan for the smallest key index in the remaining items
 				for (int j = i + 1; j < input.size(); j++) {
 					if (input.get(j).compareTo(input.get(i)) < 0) {
 						index = j;
 					}
 				}
+				// swap smallest item with item at current index
 				temp = input.get(i);
 				input.set(i, input.get(index));
 				input.set(index, temp);
